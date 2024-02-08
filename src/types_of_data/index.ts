0c105b2813a9e65f4c5939d1874ef6c58a4bdb79
magnifiq <xbindiuk@stuba.sk>
2024-02-08 15:46:32 +0100

    add data types for database

---
+++ src/types_of_data/index.ts
@@ -0,0 +1,26 @@
+export interface ClientSchema {
+  id?: string;
+  username: string;
+  age: number;
+  hobbies: string[] | [];
+}
+
+export interface ResponseError extends Error {
+  code?: string;
+  message: string;
+}
+
+export interface IncomingBody {
+  body?: ClientSchema;
+  params?: object;
+}
+
+export interface MainDoc {
+  id?: string;
+}
+
+export interface MainCol {
+  collectionName: string;
+  collectionSchema: { required: Array<string>; types: Record<string, any> };
+  store: object;
+}
