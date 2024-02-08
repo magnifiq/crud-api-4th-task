2fb7c556b664d9565bc711d13239842343bd3e16
magnifiq <xbindiuk@stuba.sk>
2024-02-08 16:18:21 +0100

    add user model of database

---
+++ src/database/index.ts
@@ -0,0 +1,25 @@
+import Schema from "./Schema";
+import Database from './Database';
+import { ClientSchema } from '../types_of_data';
+
+export const userRequired: Array<keyof ClientSchema> = ['username', 'age', 'hobbies'];
+
+export const userFieldsTypes = {
+  username: 'string',
+  age: 'number',
+  hobbies: ['string'],
+};
+
+const store = {};
+const UserModel = new Schema<ClientSchema>(
+  "users",
+  {
+    required: userRequired,
+    types: userFieldsTypes,
+  },
+  store
+);
+
+export const db = new Database([UserModel], store);
+
+export default UserModel;
