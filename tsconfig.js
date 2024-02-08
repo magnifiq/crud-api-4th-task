92d3629d560d86cb78068d83706686629428ba43
magnifiq <xbindiuk@stuba.sk>
2024-02-08 15:32:50 +0100

    basic setup

---
+++ tsconfig.js
@@ -0,0 +1,22 @@
+{
+    "compilerOptions": {
+        "target": "es6",
+            "module": "commonJs",
+                "declaration": true,
+                    "sourceMap": true,
+                        "outDir": "./build",
+                            "strict": true,
+                                "esModuleInterop": true,
+                                    "skipLibCheck": true,
+                                        "forceConsistentCasingInFileNames": true,
+                                            "resolveJsonModule": true,
+                                                "noImplicitAny": false
+    },
+    "exclude": [
+        "node_modules",
+        "build"
+    ],
+        "include": [
+            "src"
+        ]
+}
